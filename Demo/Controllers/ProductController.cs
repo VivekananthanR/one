82df3de8e69a389396f3e73fa89776ed9a9880f2
VivekananthanR <vivekananthanvivek12345@gmail.com>
2025-09-09 17:08:43 +0530

    Add project files.

---
+++ Demo/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private static readonly List<string> products = new()
+        {
+            "Laptop",
+            "Mobile",
+            "Tablet"
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+    }
+}
